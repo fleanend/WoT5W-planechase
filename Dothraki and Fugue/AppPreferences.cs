f48a6c98133b0b4359b21c364510fb7756f103be
fleanend <edoardoferrante@hotmail.it>
2019-05-02 14:07:44 +0200

    Add AppPreferences Stub

---
+++ Dothraki and Fugue/AppPreferences.cs
@@ -0,0 +1,31 @@
+﻿using Android.Content;
+using Android.Preferences;
+
+public class AppPreferences
+{
+    private ISharedPreferences mSharedPrefs;
+    private ISharedPreferencesEditor mPrefsEditor;
+    private Context mContext;
+
+    private static string PREFERENCE_ACCESS_KEY = "PREFERENCE_ACCESS_KEY";
+
+    public AppPreferences(Context context)
+    {
+        this.mContext = context;
+        mSharedPrefs = PreferenceManager.GetDefaultSharedPreferences(mContext);
+        mPrefsEditor = mSharedPrefs.Edit();
+    }
+
+    public void saveAccessKey(string key)
+    {
+        mPrefsEditor.PutString(PREFERENCE_ACCESS_KEY, key);
+        mPrefsEditor.Commit();
+    }
+
+    public string getAccessKey()
+    {
+        return mSharedPrefs.GetString(PREFERENCE_ACCESS_KEY, "");
+    }
+
+
+}
